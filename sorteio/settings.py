b99ec34607557c6790cb62d16f1f322466bd6261
Eduardo Lucas <eduardolucas40@gmail.com>
2019-08-29 13:53:37 -0300

    last commit before deployment at Heroku

--- sorteio/settings.py
+++ sorteio/settings.py
@@ -147,10 +147,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
+PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "static"),
+    os.path.join(PROJECT_ROOT, "static"),
 ]
 
 LOGIN_REDIRECT_URL = '/'
@@ -172,4 +174,5 @@ MESSAGE_TAGS = {
 
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 
